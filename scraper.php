479c760edb188f3e4cb59815de8df390b74fb8bc
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-24 12:20:53 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,27 +1,13 @@
 <?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
-
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+$link		=	'http://www.indianyellowpages.com/search.php?term=pvt&pageno=0';
+		$page		=	file_get_html($link);
+		$totalrec	=	$page->find("//*[@id='breadcrumb']/li[3]",0);
+		$text = str_replace(' Result(s) Found', '', $totalrec);
+		$records = str_replace('- ', '', $text);
+		$paginations = $records  / 50 + 1;
+		$pages =  (int)$paginations;
+		echo $paginations;
 ?>
