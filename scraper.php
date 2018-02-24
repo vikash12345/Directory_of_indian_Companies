f7dcae81959bc0a8a01be054f3186a50b595c817
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-24 12:43:06 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,12 +2,29 @@
 
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
-		link		=	'http://www.indianyellowpages.com/search.php?term=pvt&pageno=0';
+		$link		=	'http://www.indianyellowpages.com/search.php?term=pvt&pageno=0';
 		$page		=	file_get_html($link);
 		$totalrec	=	$page->find("//*[@id='breadcrumb']/li[3]",0)->plaintext;
 		$text = str_replace(' Result(s) Found', '', $totalrec);
 		$records = str_replace('- ', '', $text);
 		$paginations = $records  / 50;
 		$pages =  (int)$paginations;
-		echo $pages;
+		
+	
+for($page = 0;$page <= 0; $page++)
+	{
+		$link	=	'http://www.indianyellowpages.com/search.php?term=pvt&pageno='.$page;
+		$html	=	file_get_html($link);
+		if($html)
+		{
+			foreach($html->find("/html/body/div/div/div[@class='mb15px fo bdr bsb5px10-hover']") as $element)
+			{
+				$nameofcompany	=	$element->find("a[@class='xxlarge']",0)->plaintext;
+				$linkofcompany	=	$element->find("a[@class='xxlarge']",0)->href;
+				echo $linkofcompany;
+			}
+		}
+	
+	}
+
 ?>
