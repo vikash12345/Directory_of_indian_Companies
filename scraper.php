12f7c44defc452bdbf34c9daf969fb0e9ee5e196
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-24 12:22:11 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -4,7 +4,7 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 $link		=	'http://www.indianyellowpages.com/search.php?term=pvt&pageno=0';
 		$page		=	file_get_html($link);
-		$totalrec	=	$page->find("//*[@id='breadcrumb']/li[3]",0);
+		$totalrec	=	$page->find("//*[@id='breadcrumb']/li[3]",0)->plaintect;
 		$text = str_replace(' Result(s) Found', '', $totalrec);
 		$records = str_replace('- ', '', $text);
 		$paginations = $records  / 50 + 1;
