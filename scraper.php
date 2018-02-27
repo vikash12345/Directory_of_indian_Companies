5f775b47bf15ef05721ddae568bf0eaef0333a86
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-27 10:26:35 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,7 +2,7 @@
 
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
-		$link		=	'http://www.indianyellowpages.com/search.php?term=pvt&pageno=0';
+		$link		=	'http://www.indianyellowpages.com/search.php?term=ltd&pageno=0';
 		$page		=	file_get_html($link);
 		$totalrec	=	$page->find("//*[@id='breadcrumb']/li[3]",0)->plaintext;
 		$text = str_replace(' Result(s) Found', '', $totalrec);
@@ -13,18 +13,18 @@ require 'scraperwiki/simple_html_dom.php';
 	
 for($page = 0;$page <= 0; $page++)
 	{
-		$link	=	'http://www.indianyellowpages.com/search.php?term=pvt&pageno='.$page;
+		$link	=	'http://www.indianyellowpages.com/search.php?term=ltd&pageno='.$page;
 		$html	=	file_get_html($link);
-		if($html)
+		$link	=	$html->find("a[@class='xxlarge']",0)->href;
+		echo $link;
+		/*if($html)
 		{
+			sleep(5);
 			foreach($html->find("/html/body/div/div/div[@itemtype='http://schema.org/Organization']") as $element)
 			{
-				$nameofcompany	=	$element->find("a[@class='xxlarge']",0)->href;
-				
-				echo $nameofcompany'<br>';
-				scraperwiki::save_sqlite(array('link'), array('link' => $nameofcompany));
+				echo $element;
 			}
-		}
+		}*/
 	
 	}
 ?>
